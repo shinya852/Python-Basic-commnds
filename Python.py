818c956a92b0b3491a3da8442533db7dd9e6a82e
shinya852 <shinya.june@gmail.com>
2022-02-05 12:55:03 +0900

    Use shell & Basic commands

---
+++ Python.py
@@ -0,0 +1,25 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 18:41:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print(1+1)
+2
+>>> print(1000-259)
+741
+>>> print(65982*35)
+2309370
+>>> print(85/3)
+28.333333333333332
+>>> print(85//3)
+28
+>>> print(85%3)
+1
+>>> print("ようこそ")
+ようこそ
+>>> print('Hellow')
+Hellow
+>>> print("1-9")
+1-9
+>>> print("答えは", 50+80)
+答えは 130
+>>> print("私は'こんにちは'と答えた")
+私は'こんにちは'と答えた
+>>> 
