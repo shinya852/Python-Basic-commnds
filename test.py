588a58ddd9d632915ee5083bca3262767546c6d7
shinya852 <shinya.june@gmail.com>
2022-02-14 13:23:08 +0900

    compleate & go next repository

---
+++ test.py
@@ -0,0 +1,35 @@
+a = 10
+print(a)
+
+h = 1.71
+w = 64
+bmi = w / (h * h)
+print(bmi)
+
+i = 100
+f = 12.3
+w = "hellow"
+b = True
+print(i, f, w, b)
+
+w = "こんにちは。" + "私はパイソンです。"
+print(len(w))
+
+w = "こんにちは。" + "私はパイソンです。"
+print(w[0])
+print(w[6:12])
+print(w[-3:])
+
+w = "こんにちは。" +  "\n" + "私はパイソンです。"
+print(w)
+
+a = "100"
+print(int(a)+23)
+
+a = "100"
+b = "こんにちは"
+print(a.isdigit())
+print(b.isdigit())
+
+lunch = ["おにぎり", "パスタ", "ハンバーガー", "カレー", "定食"]
+print(lunch[2])
